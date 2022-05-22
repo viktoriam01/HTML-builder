93ed7dbfba59ecd7ef3211d5f35f22730590ad40
Viktoria Motoshkina <aislingdar@gmail.com>
2022-05-23 02:40:48 +0800

    feat: 04-copy-directory done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,48 +3,23 @@ const path = require('path');
 const folderName = path.join(__dirname, 'files');
 const copyFolderName = path.join(__dirname, 'files-copy');
 
-
-if (!fs.exists(copyFolderName)) {
-    fs.mkdir(copyFolderName, { recursive: true }, (err) => {
-        if (err) {
-            console.error(err)
-            return
-        }
-    })
-   
-} else {
-
-     fs.readdir(copyFolderName, (err, files) => {
-        if (err) {
+fs.mkdir(copyFolderName, { recursive: true }, (err) => {
+    if (err) {
         console.error(err)
         return
-        }
+    }
+})
+fs.readdir(copyFolderName, (err, files) => {
         files.forEach(file => {
-            fs.unlink(path.join(copyFolderName, file), (err) => {
-                if (err) throw err;
-                console.log('Файл успешно удален');
-            });
+            fs.promises.unlink(path.join(copyFolderName, file))
+            console.log('Файл успешно удален');
         })
     })
-}
-
-
-
-// fs.mkdir(copyFolderName, { recursive: true }, (err) => {
-//     if (err) {
-//         console.error(err)
-//         return
-//     }
-// })
-
 fs.readdir(folderName, (err, files) => {
     if (err) {
       console.error(err)
       return
-    }
-   
-    
-
+    }   
     files.forEach(file => {
         fs.copyFile(path.join(folderName, file), path.join(copyFolderName, file), (err) => {
             if (err) {
@@ -55,3 +30,17 @@ fs.readdir(folderName, (err, files) => {
         });
     })
 })
+
+
+// if (!fs.access(copyFolderName)) {
+//     fs.mkdir(copyFolderName, { recursive: true }, (err) => {
+//         if (err) {
+//             console.error(err)
+//             return
+//         }
+//     })
+   
+// } else {
+
+     
+// }
