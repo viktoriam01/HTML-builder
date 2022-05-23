5c7e70c24809b717bb354ed2961b4d43b921cf30
Viktoria <aislingdar@gmail.com>
2022-05-23 10:51:48 +0800

    feat: fix 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,46 +1,60 @@
-const fs = require('fs');
 const path = require('path');
 const folderName = path.join(__dirname, 'files');
 const copyFolderName = path.join(__dirname, 'files-copy');
+const { mkdir, readdir, copyFile, unlink } = require('fs/promises');
 
-fs.mkdir(copyFolderName, { recursive: true }, (err) => {
-    if (err) {
-        console.error(err)
-        return
-    }
-})
-fs.readdir(copyFolderName, (err, files) => {
+const copyDir = () => mkdir(copyFolderName, { recursive: true });
+
+const cleanFolder = () => {
+    readdir(copyFolderName).then(files => {
         files.forEach(file => {
-            fs.promises.unlink(path.join(copyFolderName, file))
+            unlink(path.join(copyFolderName, file))
             console.log('Файл успешно удален');
         })
     })
-fs.readdir(folderName, (err, files) => {
-    if (err) {
-      console.error(err)
-      return
-    }   
-    files.forEach(file => {
-        fs.copyFile(path.join(folderName, file), path.join(copyFolderName, file), (err) => {
-            if (err) {
-                console.error(err)
-                return
-            }
+}
+const copyFiles = () => {
+    readdir(folderName).then(files => {
+        files.forEach(file => {
+        copyFile(path.join(folderName, file), path.join(copyFolderName, file))
             console.log('Файл успешно копирован')
-        });
+        })
     })
-})
+    
+}
+
+copyDir()
+.then(() => cleanFolder())
+.then(() => copyFiles())
+.catch(error => {throw error});
 
 
-// if (!fs.access(copyFolderName)) {
-//     fs.mkdir(copyFolderName, { recursive: true }, (err) => {
-//         if (err) {
-//             console.error(err)
-//             return
-//         }
+// fs.mkdir(copyFolderName, { recursive: true }, (err) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+// })
+// fs.readdir(copyFolderName, (err, files) => {
+//         files.forEach(file => {
+//             fs.promises.unlink(path.join(copyFolderName, file))
+//             console.log('Файл успешно удален');
+//         })
 //     })
-   
-// } else {
+// fs.readdir(folderName, (err, files) => {
+//     if (err) {
+//       console.error(err)
+//       return
+//     }   
+//     files.forEach(file => {
+//         fs.copyFile(path.join(folderName, file), path.join(copyFolderName, file), (err) => {
+//             if (err) {
+//                 console.error(err)
+//                 return
+//             }
+//             console.log('Файл успешно копирован')
+//         });
+//     })
+// })
 
-     
-// }
+
